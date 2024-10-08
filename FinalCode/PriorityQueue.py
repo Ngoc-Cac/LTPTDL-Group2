9e2af8261eb6c4b28feaaf46051ef4308a65616e
Trịnh Ngọc Các <cactrinh5@gmail.com>
2024-10-08 16:29:49 +0700

    Resolved conflict with main branch

--- FinalCode/PriorityQueue.py
+++ FinalCode/PriorityQueue.py
@@ -5,9 +5,7 @@ from typing import TypeVar, Generic, Iterator,\
 T = TypeVar('T')
 
 
-REMOVED_FLAG: bool = True
-
-# Item = [item, count, is_removed]
+# item = [item, count, is_removed]
 # count used for FIFO structuring when priority is equal
 
 def counter() -> Iterator[int]:
@@ -23,26 +21,14 @@ class PriorityQueue(Generic[T]):
     - Sortable: must implement a `__lt__` method.
     - Hashable: must implement a `__hash__` method.
     """
-    __slots__ = "_min_heap", "_items_list", "_counter", "_len"
+    __slots__ = "_min_heap", "_items_list", "_counter"
     def __init__(self, items: Optional[Iterable[T]] = None) -> None:
         self._min_heap: list[list[T, int, bool]] = []
         self._items_list: dict[T, list[T, int, bool]] = {}
         self._counter = counter()
-        self._len = 0
 
         if items:
             for item in items: self.push(item)
-    
-    def _remove_item(self, item: T) -> None:
-        """
-        Flag an item as being removed.\n
-
-        ------------\n
-        ## Exceptions:
-        Raise KeyError if item is not found in queue.
-        """
-        self._items_list[item][2] = REMOVED_FLAG
-        self._len -= 1
     
     def push(self, item: T) -> None:
         """
@@ -55,12 +41,13 @@ class PriorityQueue(Generic[T]):
         - Item is not hashable (do not have a `__hash__` method).
         """
         # Remove item if already exists
-        if item in self._items_list: self._remove_item(item)
+        if item in self._items_list:
+            # Flag item as removed
+            self._items_list[item][2] = True
 
         new_item = [item, next(self._counter), False]
         self._items_list[item] = new_item
         hq.heappush(self._min_heap, new_item)
-        self._len += 1
 
     def pop(self) -> T:
         """
@@ -73,7 +60,6 @@ class PriorityQueue(Generic[T]):
         while self._min_heap:
             item, _, is_removed = hq.heappop(self._min_heap)
             if not is_removed:
-                self._len -= 1
                 del self._items_list[item]
                 return item
         raise IndexError("Queue is empty")
@@ -93,10 +79,9 @@ class PriorityQueue(Generic[T]):
         Clear the queue.
         """
         self._min_heap.clear()
-        self._len = 0
         self._counter = counter()
 
 
-    def __len__(self) -> int: return self._len
+    def __len__(self) -> int: return len(self._items_list)
 
-    def __bool__(self) -> bool: return self._len != 0
+    def __bool__(self) -> bool: return len(self) != 0
