d3aa9366905f7e1936b104705bc982f6e76f58ac
Trịnh Ngọc Các <cactrinh5@gmail.com>
2024-11-11 12:43:14 +0700

    first commit: expand_cell and cost
    expand_cell method provides 8 actions (9 if cell is dirty) which are up, down, left, right and top/down left/right. If cell is dirty, another action is suck.
    
    cost now updates sequentially as we move from one state to another.

--- A-star/Cell.py
+++ A-star/Cell.py
@@ -1,6 +1,6 @@
 from math import inf
 
-from typing import Union, Optional, Iterable
+from typing import Union, Optional, Iterable, Literal
 
 numeric = Union[int, float]
 
@@ -75,18 +75,17 @@ class Position(tuple):
         """The y-coordinate of position"""
         return self[1]
 
+ACTIONS = {'move', 'suck'}
 
 class Cell:
-    __slots__ = 'position', 'parent', 'grid_dim', 'dirty_cells', 'moves', 'cost', 'heuristic_cost'
+    __slots__ = 'position', 'parent', 'grid_dim', 'dirty_cells', 'moves', 'action', 'cost', 'heuristic_cost'
     def __init__(self, position: Position, grid_dim: tuple[int, int],
                  dirty_cells: Iterable[Position], moves: int = 0,
-                 parent: Optional['Cell'] = None,) -> None:
+                 parent: Optional['Cell'] = None, action: Literal['move', 'suck'] = 'move') -> None:
         if not isinstance(position, Position):
             raise TypeError("position must be of type Position")
         elif (position.x < 1) or (position.y < 1):
             raise ValueError("position of cell must be larger than (1, 1)")
-        if not isinstance(parent, Optional[Cell]):
-            raise TypeError("parent must be of type Cell or None")
         if not isinstance(grid_dim, tuple):
             raise TypeError("grid_dim must be a tuple of int representing the number of rows and cols")
         elif not all(isinstance(value, int) for value in grid_dim):
@@ -95,11 +94,18 @@ class Cell:
             raise ValueError(f"Invalid grid dimensions {grid_dim}")
         if not isinstance(moves, int):
             raise TypeError("moves must be a positive integer")
+        if not isinstance(parent, Optional[Cell]):
+            raise TypeError("parent must be of type Cell or None")
+        if not isinstance(action, str):
+            raise TypeError("action must be a string literal of 'move' or 'suck'")
+        elif not action in ACTIONS:
+            raise ValueError("action msut be a string literal of 'move' or 'suck'")
 
         self.position = position
         self.parent = parent
         self.grid_dim = grid_dim
         self.moves = moves
+        self.action = action
 
         self.dirty_cells = set()
         for cell_pos in dirty_cells:
@@ -112,10 +118,38 @@ class Cell:
     def expand_cell(self) -> list['Cell']:
         neighbours = []
 
-        for new_pos in self.dirty_cells:
-            neighbours.append(Cell(new_pos, parent=self, grid_dim=self.grid_dim,
-                                  dirty_cells=self.dirty_cells - {new_pos},
-                                  moves=self.moves + distance(self.position, new_pos, p=inf)))
+        # suck if dirty
+        if self.position in self.dirty_cells:
+            neighbours.append(Cell(self.position, parent=self, grid_dim=self.grid_dim,
+                                   dirty_cells=self.dirty_cells - {self.position},
+                                   moves = self.moves, action='suck'))
+        
+        neigh_pos = []
+        # up, down, left, right
+        if self.position.x > 1:
+            neigh_pos.append((self.position.x - 1, self.position.y))
+        if self.position.x < self.grid_dim[1]:
+            neigh_pos.append((self.position.x + 1, self.position.y))
+        if self.position.y > 1:
+            neigh_pos.append((self.position.x, self.position.y - 1))
+        if self.position.y < self.grid_dim[0]:
+            neigh_pos.append((self.position.x, self.position.y + 1))
+
+        # top/bot left/right
+        if (self.position.x > 1) and (self.position.y > 1):
+            neigh_pos.append((self.position.x - 1, self.position.y - 1))
+        if (self.position.x > 1) and (self.position.y < self.grid_dim[0]):
+            neigh_pos.append((self.position.x - 1, self.position.y + 1))
+        if (self.position.x < self.grid_dim[1]) and (self.position.y > 1):
+            neigh_pos.append((self.position.x + 1, self.position.y - 1))
+        if (self.position.x < self.grid_dim[1]) and (self.position.y < self.grid_dim[0]):
+            neigh_pos.append((self.position.x + 1, self.position.y + 1))
+
+        for new_pos in neigh_pos:
+            neighbours.append(Cell(Position(*new_pos), parent=self,
+                                   grid_dim=self.grid_dim,
+                                   dirty_cells=self.dirty_cells,
+                                   moves=self.moves + 1))
         return neighbours
     
 
@@ -126,12 +160,8 @@ class Cell:
     def _cost(self) -> None:
         if self.parent is None: self.cost = 0
         else:
-            # previous cost +
-            # cost to go from previous state to this state +
-            # cost to clean the cell after n movements
-            self.cost = self.parent.cost +\
-                        distance(self.parent.position, self.position, p=inf) +\
-                        self.moves + 1
+            if self.action == 'move': self.cost = self.parent.cost + 1
+            else: self.cost = self.parent.cost + self.moves + 1
     
     def _heu_cost(self) -> None:
         self.heuristic_cost = 0
